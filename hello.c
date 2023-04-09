7b87f031b1ba33742343cf13eb3ded733167852f
yuyang <1606394201@qq.com>
2023-04-09 20:26:21 +0800

    2023年4月9日20:26:05

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int i = 0;
+    i++;
+    //环境可以找到可以不用加路径
+   // system("mspaint"); //启动画图板
+    system("D:/desktop/test_folder/hello.exe");
+    printf("hello worldgsdg\n");
+    return 0;
+}
